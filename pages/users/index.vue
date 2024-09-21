07644d08b567539c7f4f705a978ef84166cfa36e
keisuke watanabe <kskwtnb93@gmail.com>
2024-09-21 20:43:32 +0900

    usersページの追加

---
+++ pages/users/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="users">
+    <NDataTable
+      remote
+      :data="users"
+      :columns="columns"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NDataTable, type DataTableColumns } from 'naive-ui'
+import type { User } from '~/types/user'
+
+const { data: users } = useFetch<User[]>('https://jsonplaceholder.typicode.com/users')
+const columns = computed<DataTableColumns<User>>(() => [
+  {
+    key: 'id',
+    title: 'ID',
+    render: row => row.id,
+  },
+  {
+    key: 'name',
+    title: 'Name',
+    render: row => row.name,
+  },
+  {
+    key: 'email',
+    title: 'Email',
+    render: row => row.email,
+  },
+  {
+    key: 'website',
+    title: 'Website',
+    render: row => row.website,
+  },
+])
+</script>
