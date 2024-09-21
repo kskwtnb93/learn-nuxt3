4dfe8d985d0df972a4f9395dfd2f37351c1b44cc
keisuke watanabe <kskwtnb93@gmail.com>
2024-09-21 17:22:58 +0900

    scssに

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-
+  css: ['~/assets/css/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
