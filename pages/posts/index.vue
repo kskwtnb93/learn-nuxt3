046fdc9a1f1b37d5cb4fcf61e830dec4a7c2153b
keisuke watanabe <kskwtnb93@gmail.com>
2024-09-23 21:40:29 +0900

    Postsページの追加

---
+++ pages/posts/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="px-[40px] py-[32px] flex flex-col gap-[32px]">
+    <span class="text-[24px] font-bold">Posts</span>
+    <NDataTable
+      v-if="posts"
+      remote
+      :data="posts"
+      :columns="columns"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { DataTableColumns } from 'naive-ui'
+import type { Post } from '~/types/post'
+
+const { data: posts } = useFetch<Post[]>(
+  'https://jsonplaceholder.typicode.com/posts',
+)
+const columns = computed<DataTableColumns<Post>>(() => [
+  {
+    key: 'id',
+    title: 'ID',
+    width: 60,
+    render: row => row.id,
+  },
+  {
+    key: 'title',
+    title: 'Title',
+    render: row => row.title,
+  },
+  {
+    key: 'body',
+    title: 'Body',
+    render: row => row.body,
+  },
+])
+</script>
