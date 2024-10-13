5f4f97f094dde1ea9e86cd2635e038dacf2bd1cd
keisuke watanabe <kskwtnb93@gmail.com>
2024-10-13 18:01:50 +0900

    [WIP] ローディングとダイアログの表示

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,7 +1,24 @@
 export default defineNuxtPlugin(() => {
+  // const loading = useLoading()
+  // const dialog = useDialog()
+
   const api = $fetch.create({
     baseURL: 'https://jsonplaceholder.typicode.com/',
+    // onRequest: () => {
+    //   loading.start()
+    // },
+    // onResponse: () => {
+    //   loading.finish()
+    // },
+    // onResponseError: () => {
+    //   loading.error()
+    //   dialog.error({
+    //     title: 'エラー',
+    //     content: 'エラーが発生しました。',
+    //   })
+    // },
   })
+
   return {
     provide: {
       api,
